16c5074a26ea0b268d2f6724f2e6888fe39c6ff1
Raoul <scaliseraoul00@gmail.com>
2024-03-10 22:58:46 +0000

    Optimized commands and added header on csv generated files

--- commands.sh
+++ commands.sh
@@ -1,142 +1,33 @@
 ###Analysis
 python scripts/moving_average.py data/drawpoint/*.csv
 python scripts/moving_average.py data/movemap/*.csv
-
 python scripts/boxplot_elapsed.py data/drawpoint/*.csv
 python scripts/boxplot_elapsed.py data/movemap/*.csv
-#test
 
-sh mosquitto_marker_generator.sh 100 test
+###DrawPoint
+sh tester.sh AndroidKotlinGoogleMaps DrawPoint
+sh tester.sh AndroidKotlinMapbox DrawPoint
+sh tester.sh iOSSwiftAppleMap DrawPoint
+sh tester.sh iOSSwiftMapbox DrawPoint
+sh tester.sh AndroidFlutterMapbox DrawPoint
+sh tester.sh iOSFlutterMapbox DrawPoint
 
-###Android Kotlin
+###MoveMap
+sh tester.sh AndroidKotlinGoogleMaps MoveMap
+sh tester.sh AndroidKotlinMapbox MoveMap
+sh tester.sh iOSSwiftAppleMap MoveMap
+sh tester.sh iOSSwiftMapbox MoveMap
+sh tester.sh AndroidFlutterMapbox MoveMap
+sh tester.sh iOSFlutterMapbox MoveMap
 
-##General
+###InAppAlert
+sh tester.sh AndroidKotlin InAppAlert
+sh tester.sh iOSSwift InAppAlert
+sh tester.sh AndroidFlutter InAppAlert
+sh tester.sh iOSFlutter InAppAlert 
 
-#inAppAlert AndroidKotlinInAppAlertReceive
-sh mosquitto_inappalert_generator.sh 100 AndroidKotlinInAppAlertReceive 2
-
-mosquitto_sub -h localhost -t "AndroidKotlinInAppAlertComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinInAppAlert.csv
-
-#inAppNotification AndroidKotlinInAppNotificationReceive
-sh mosquitto_inappnotification_generator.sh 100 AndroidKotlinInAppNotificationReceive 2
-
-mosquitto_sub -h localhost -t "AndroidKotlinInAppNotificationComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinInAppNotification.csv
-
-
-##GoogleMaps
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 AndroidKotlinGoogleMapsDrawPointReceive
-sh mosquitto_marker_generator.sh 100 AndroidKotlinGoogleMapsDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "AndroidKotlinGoogleMapsDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinGoogleMapsDrawPoint.csv
-
-#moveMap
-sh mosquitto_movemap_generator.sh 100 AndroidKotlinGoogleMapsMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "AndroidKotlinGoogleMapsMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinGoogleMapsMoveMap.csv
-
-
-##MapBox
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 AndroidKotlinMapboxDrawPointReceive
-sh mosquitto_marker_generator.sh 100 AndroidKotlinMapboxDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "AndroidKotlinMapboxDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinMapboxDrawPoint.csv
-
-#moveMap
-sh mosquitto_movemap_generator.sh 100 AndroidKotlinMapboxMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "AndroidKotlinMapboxMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidKotlinMapboxMoveMap.csv
-
-###iOS Swift
-
-##General
-
-#inAppAlert iOSSwiftInAppAlertReceive
-sh mosquitto_inappalert_generator.sh 100 iOSSwiftInAppAlertReceive 2
-
-mosquitto_sub -h localhost -t "iOSSwiftInAppAlertComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftInAppAlert.csv
-
-#inAppNotification iOSSwiftInAppNotificationReceive
-sh mosquitto_inappnotification_generator.sh 100 iOSSwiftInAppNotificationReceive 2
-
-mosquitto_sub -h localhost -t "iOSSwiftInAppNotificationComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftInAppNotification.csv
-
-
-
-##Apple Maps
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 iOSSwiftAppleMapDrawPointReceive
-sh mosquitto_marker_generator.sh 100 iOSSwiftAppleMapDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "iOSSwiftAppleMapDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftAppleMapDrawPoint.csv
-
-#moveMap
-sh mosquitto_movemap_generator.sh 100 iOSSwiftAppleMapMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "iOSSwiftAppleMapMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftAppleMapMoveMap.csv
-
-##Mapbox
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 iOSSwiftMapboxDrawPointReceive
-sh mosquitto_marker_generator.sh 100 iOSSwiftMapboxDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "iOSSwiftMapboxDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftMapboxDrawPoint.csv
-
-#moveMap
-sh mosquitto_movemap_generator.sh 100 iOSSwiftMapboxMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "iOSSwiftMapboxMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSSwiftMapboxMoveMap.csv
-
-
-###FlutterMapbox
-
-##Android
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 AndroidFlutterMapboxDrawPointReceive
-sh mosquitto_marker_generator.sh 100 AndroidFlutterMapboxDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "AndroidFlutterMapboxDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidFlutterMapboxDrawPoint.csv
-
-#moveMap
-sh mosquitto_movemap_generator.sh 100 AndroidFlutterMapboxMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "AndroidFlutterMapboxMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidFlutterMapboxMoveMap.csv
-
-#inAppAlert
-sh mosquitto_inappalert_generator.sh 100 AndroidFlutterInAppAlertReceive 2
-
-mosquitto_sub -h localhost -t "AndroidFlutterInAppAlertComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidFlutterInAppAlert.csv
-
-#inAppNotification AndroidFlutterInAppNotificationReceive
-sh mosquitto_inappnotification_generator.sh 100 AndroidFlutterInAppNotificationReceive 2
-
-mosquitto_sub -h localhost -t "AndroidFlutterInAppNotificationComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > AndroidFlutterInAppNotification.csv
-
-##iOS
-
-#drawPoint
-sh mosquitto_marker_generator.sh 100 iOSFlutterMapboxDrawPointReceive
-sh mosquitto_marker_generator.sh 100 iOSFlutterMapboxDrawPointReceive 1
-
-mosquitto_sub -h localhost -t "iOSFlutterMapboxDrawPointComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSFlutterMapboxDrawPoint.csv
-
-#moveMap  -- iOSFlutterMapboxmoveMapReceive
-sh mosquitto_movemap_generator.sh 100 iOSFlutterMapboxMoveMapReceive 2
-
-mosquitto_sub -h localhost -t "iOSFlutterMapboxMoveMapComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSFlutterMapboxMoveMap.csv
-
-#inAppAlert iOSFlutterInAppAlertReceive
-sh mosquitto_inappalert_generator.sh 100 iOSFlutterInAppAlertReceive 2
-
-mosquitto_sub -h localhost -t "iOSFlutterInAppAlertComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSFlutterInAppAlert.csv
-
-#inAppNotification iOSFlutterInAppNotificationReceive
-sh mosquitto_inappnotification_generator.sh 100 iOSFlutterInAppNotificationReceive 2
-
-mosquitto_sub -h localhost -t "iOSFlutterInAppNotificationComplete" | while read line; do echo "$line,$(gdate +%s%3N)"; done > iOSFlutterInAppNotification.csv
+###InAppNotification
+sh tester.sh AndroidKotlin InAppNotification
+sh tester.sh iOSSwift InAppNotification
+sh tester.sh AndroidFlutter InAppNotification
+sh tester.sh iOSFlutter InAppNotification 
---
+++ tester.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <topic> <event_type>"
+  exit 1
+fi
+
+TOPIC=$1
+EVENT_TYPE=$2
+FILE=""
+INTERVAL=2
+
+pkill -x mosquitto_sub
+
+case $EVENT_TYPE in
+  InAppAlert)
+    echo "Handling InAppAlert for topic $TOPIC$EVENT_TYPE"
+    FILE="mosquitto_inappalert_generator.sh"
+    ;;
+  InAppNotification)
+    echo "Handling InAppNotification for topic $TOPIC$EVENT_TYPE"
+    FILE="mosquitto_inappnotification_generator.sh"
+    ;;
+  DrawPoint)
+    echo "Handling DrawPoint for topic $TOPIC$EVENT_TYPE"
+    FILE="mosquitto_drawpoint_generator.sh"
+    ;;
+  MoveMap)
+    echo "Handling MoveMap for topic $TOPIC$EVENT_TYPE"
+    FILE="mosquitto_movemap_generator.sh"
+    ;;
+  *)
+    echo "Error: Invalid event type. Please use one of the following: InAppAlert, InAppNotification, DrawPoint, MoveMap."
+    exit 2
+    ;;
+esac
+
+echo "Generating 1 event every $INTERVAL seconds"
+
+BACKUP_DIR="backup"
+FILE_PATH="${TOPIC}${EVENT_TYPE}.csv"
+BACKUP_FILE="${BACKUP_DIR}/${TOPIC}${EVENT_TYPE}_$(gdate +%s).csv"
+
+if [ -f "$FILE_PATH" ]; then
+  mkdir -p "$BACKUP_DIR"
+  mv "$FILE_PATH" "$BACKUP_FILE"
+fi
+
+echo "TIMESTAMP_SENT,OS,LANGUAGE,MAP,EVENT,CONF,LATENCY,ELAPSED_TIME,TIMESTAMP_RECEIVED" > $FILE_PATH
+mosquitto_sub -h localhost -t "${TOPIC}${EVENT_TYPE}Complete" | while read line; do echo "$line,$(gdate +%s%3N)"; done >> $FILE_PATH &
+
+
+sh $FILE 100 ${TOPIC}${EVENT_TYPE}Receive $INTERVAL
+
+exit 0
